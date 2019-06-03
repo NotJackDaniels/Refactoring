b3a94d24be341550973745831460d0ac938cd75f
MarkKislov <mark-kislov@inbox.ru>
2019-06-03 19:04:00 +0700

    up

---
+++ Game/Documents/Game/Assets/Script/AnimationHero.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHero : MonoBehaviour {
+
+    Animator animator;
+
+    float vertical;
+    float horizontal;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+		
+		if (vertical == 0 && horizontal == 0) 
+		{
+            animator.SetBool("Walk", false);
+		}
+        else if (vertical >= 0.1f || vertical <= -0.1f || horizontal >= 0.1f || horizontal <= -0.1f)
+        {
+            animator.SetBool("Walk", true);
+		}
+
+		if (Input.GetKey (KeyCode.F)) {
+			animator.SetTrigger ("Attack");
+			animator.SetBool ("Walk", false);
+		}
+		
+    }
+}
