20cfb07021372f7c4d43619463dcabe492dbe693
daniel-isme <46986284+daniel-isme@users.noreply.github.com>
2019-06-05 21:18:55 +0700

    Объединение всего, работаем осюда

---
+++ GameRPG/Documents/GameRPG/Assets/Script/ThirdPersonCharacterController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public float Speed;
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver).normalized * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
