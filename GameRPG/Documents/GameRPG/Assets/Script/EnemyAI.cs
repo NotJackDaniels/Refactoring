54e5a834ea5d7e0ab7824e82408481394d07fad6
daniel-isme <46986284+daniel-isme@users.noreply.github.com>
2019-06-07 19:47:48 +0700

    Анимация врага

--- GameRPG/Documents/GameRPG/Assets/Script/EnemyAI.cs
+++ GameRPG/Documents/GameRPG/Assets/Script/EnemyAI.cs
@@ -9,23 +9,34 @@ public class EnemyAI : MonoBehaviour {
     public float speed;
     private Transform target;
     public int healthMonster;
-    // Use this for initialization
+    Animator animator;
+
+
     void Start()
     {
+        animator = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (Vector3.Distance(transform.position, target.transform.position) < seeDistance)
         {
+            transform.LookAt(target.transform);
             if (Vector3.Distance(transform.position, target.transform.position) < attackDistance)
             {
-                transform.LookAt(target.transform);
+                animator.SetBool("Walk", true);
                 transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
-                Debug.Log(healthMonster);
             }
+            else
+            {
+                animator.SetBool("Walk", false);
+            }
+        }
+        else
+        {
+            animator.SetBool("Walk", false);
         }
     }
 }
---
+++ GameRPG/Documents/GameRPG/Assets/Script/PlayerAnimationScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationScript : MonoBehaviour {
+    public GameObject triggerDamage;
+    Animator animator;
+
+    float vertical;
+    float horizontal;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+		
+		if (vertical == 0 && horizontal == 0) 
+		{
+            animator.SetBool("Walk", false);
+
+		}
+        else
+        {
+            animator.SetBool("Walk", true);
+		}
+
+		if (Input.GetKeyDown (KeyCode.F)) {
+			animator.SetBool ("Walk", false);
+			animator.SetTrigger ("Attack");
+            triggerDamage.SetActive(true);
+		}
+		
+    }
+}
