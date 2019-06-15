3605bd9cc087823ef4676797fc9a295cf1243c7d
MarkKislov <mark-kislov@inbox.ru>
2019-06-16 01:06:19 +0700

    анимированный сундуки с деньгами внутри

---
+++ GameRPG/Documents/GameRPG/Assets/Script/Chest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour {
+
+    public int EnemyCoins;
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            animator.SetBool("openChest", true);
+
+            money_player.money += EnemyCoins; // добавляем монеты в указанную ссылку
+            GameObject.FindGameObjectWithTag("Player").GetComponent<money_player>().TextMoney.text = money_player.money.ToString(); // вывод количеста монет на экран
+            EnemyCoins = 0;
+        }
+    }
+}
