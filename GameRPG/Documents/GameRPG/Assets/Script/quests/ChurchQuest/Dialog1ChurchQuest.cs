86760facd82e166b3b8f4f384f8453a87695cb71
Litvinovich Ilya <ltvnvchil@gmail.com>
2019-06-21 03:44:25 +0700

    третийквест

---
+++ GameRPG/Documents/GameRPG/Assets/Script/quests/ChurchQuest/Dialog1ChurchQuest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog1ChurchQuest : MonoBehaviour {
+
+    public GameObject Text;
+    public GameObject Text1;
+
+    public short isTextNumber;
+    public ChurchQuestNpcDialog churchQuestNpcDialog;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (churchQuestNpcDialog.Dialog1 == true)
+        {
+            isTextNumber = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isTextNumber++;
+        }
+
+        switch (isTextNumber)
+        {
+            case 0:
+                Text.SetActive(true);
+                break;
+            case 1:
+                Text1.SetActive(true);
+                Text.SetActive(false);
+                break;
+            default:
+                Text1.SetActive(false);
+                churchQuestNpcDialog.EndDialogs1 = true;
+                Time.timeScale = 1;
+                churchQuestNpcDialog.Dialog1.SetActive(false);
+                break;
+        }
+    }
+}
---
+++ GameRPG/Documents/GameRPG/Assets/Script/quests/ChurchQuest/Dialog2ChurchQuest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog2ChurchQuest : MonoBehaviour {
+
+    public GameObject Text;
+
+    public short isTextNumber;
+    public ChurchQuestNpcDialog churchQuestNpcDialog;
+
+    // Use this for initialization
+    void Start()
+    {
+        Text.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Text.SetActive(false);
+            churchQuestNpcDialog.EndDialogs2 = true;
+            churchQuestNpcDialog.Dialog2.SetActive(false);
+        }   
+    }        
+}
