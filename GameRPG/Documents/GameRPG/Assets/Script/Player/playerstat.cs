232023b3bf0ae83b95b37eb391e62a70937023ee
MarkKislov <mark-kislov@inbox.ru>
2019-06-21 05:31:49 +0700

    ап статов при увеличении уровня, подправил баланс, мелочи

--- GameRPG/Documents/GameRPG/Assets/Script/Player/playerstat.cs
+++ GameRPG/Documents/GameRPG/Assets/Script/Player/playerstat.cs
@@ -43,39 +43,22 @@ public class playerstat : MonoBehaviour
             curMP = 0; //Ставим 0 
         if (curMP > stats.MP) //если кол-во маны больше максимального кол-ва 
             curMP = stats.MP;//уравниваем 
-
-        if (showstat == 0) //Если окно со статами не отображается 
-        {
-            if (Input.GetKeyDown(KeyCode.P)) //Принажатии на клавишу P 
-                showstat = 1; //окно со статами будет отображаться 
-        }
-        else if (showstat == 1) //если окно со статами отображается 
-        {
-            if (Input.GetKeyDown(KeyCode.P)) //При нажатии на клавишу P 
-                showstat = 0; //Окно исчезнет 
-        }
-
-        if (true)//movePlayer.IsDrawWeapon == true) //Проверяем вытащено ли у нас оружие, и если вытащено 
-        {
-            stats.minATKweapon = weaponstat.minATKweapon; //Устанавливаем значение минимальной атаки оружия 
-            stats.maxATKweapon = weaponstat.maxATKweapon; //Максимальной атаки оружия 
-            stats.newdmg(); //Пересчитываем урон согласно оружию 
-        }
-        else //если не одето 
-        {
-            stats.minATKweapon = 0; //ставим значения в 0 
-            stats.maxATKweapon = 0;
-            stats.newdmg(); //соответственно атака у нас 0 
-        }
+        
 
         if (curEXP >= stats.EXP) //Если количество опыта у нас рано и ли больше нужного кол-ва опыта 
         {
             lvl++;
+
+            stats.HP += 20;
+            curHP = stats.HP;
+
+            stats.MP += 5;
+            curMP = stats.MP;
+
             GameObject.FindGameObjectWithTag("Player").GetComponent<lvl_player>().player_LVL.text = lvl.ToString();
 
             stats.lvlUP(); //повышаем уровень 
-            curEXP = 0; //кол-во опыта ставим 0 
-            pointstat += 5; //Добавляем очки статов 
+            curEXP = 0; //кол-во опыта ставим 0             
         }
 
         // бутылки со здоровьем
@@ -111,54 +94,6 @@ public class playerstat : MonoBehaviour
     }
     void OnGUI()
     {
-        if (showstat == 1) //если статы отображаются 
-        {
-            //Рисуем наши статы 
-            GUI.Box(new Rect(10, 70, 300, 300), "stats");
-            GUI.Label(new Rect(10, 95, 300, 300), "LvL: " + stats.lvl);
-            GUI.Label(new Rect(10, 110, 300, 300), "hp: " + stats.HP);
-            GUI.Label(new Rect(10, 125, 300, 300), "mp: " + stats.MP);
-            GUI.Label(new Rect(10, 140, 300, 300), "exp: " + stats.EXP);
-            GUI.Label(new Rect(10, 155, 300, 300), "str: " + stats.STR);
-            GUI.Label(new Rect(10, 170, 300, 300), "vitality: " + stats.vitality);
-            GUI.Label(new Rect(10, 185, 300, 300), "energy: " + stats.energy);
-            GUI.Label(new Rect(10, 200, 300, 300), "minDMG: " + stats.minDMG);
-            GUI.Label(new Rect(10, 215, 300, 300), "maxDMG: " + stats.maxDMG);
-
-            if (pointstat > 0) //если очков статов больше 0 делаем кнопки для повышения статов 
-            {
-                GUI.Label(new Rect(10, 250, 300, 20), "points " + pointstat.ToString());
-                if (GUI.Button(new Rect(150, 155, 20, 20), "+")) //Для силы 
-                {
-                    if (pointstat > 0)
-                    {
-                        pointstat -= 1;
-                        stats.STR += 1;
-                        stats.newdmg();
-                    }
-                }
-                if (GUI.Button(new Rect(150, 170, 20, 20), "+")) //Для живучести 
-                {
-                    if (pointstat > 0)
-                    {
-                        pointstat -= 1;
-                        stats.vitality += 1;
-                        stats.newhp();
-                    }
-                }
-                if (GUI.Button(new Rect(150, 185, 20, 20), "+")) //Для маны 
-                {
-                    if (pointstat > 0)
-                    {
-                        pointstat -= 1;
-                        stats.energy += 1;
-                        stats.newmp();
-                    }
-                }
-            }
-        }
-        else if (showstat == 0)
-            useGUILayout = false; //Скрываем окно статов 
         if (death == true) //Если умерли 
         {
             Cursor.visible = true;
