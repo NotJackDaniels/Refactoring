e306794ae62a2ea4485bdedd440b26ea1c573574
Litvinovich Ilya <ltvnvchil@gmail.com>
2019-06-21 14:09:15 +0700

    звук шагов

--- GameRPG/Documents/GameRPG/Assets/Script/Player/ThirdPersonCharacterController.cs
+++ GameRPG/Documents/GameRPG/Assets/Script/Player/ThirdPersonCharacterController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class ThirdPersonCharacterController : MonoBehaviour
 {
     public float Speed;
     public float speedRun;
     public float enerjyForRun;
+    public AudioSource sourse;
+    public AudioClip clip;
 
 
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        sourse.playOnAwake = false;
+        sourse.loop = true;
+        sourse.clip = clip; 
     }
 
     void Update()
@@ -27,7 +34,11 @@ public class ThirdPersonCharacterController : MonoBehaviour
             float hor = Input.GetAxis("Horizontal");
             float ver = Input.GetAxis("Vertical");
             Vector3 playerMovement = new Vector3(hor, 0f, ver).normalized * speedRun * Time.deltaTime;
-            transform.Translate(playerMovement, Space.Self);
+            transform.Translate(playerMovement, Space.Self);   
+            if (!sourse.isPlaying)
+            {
+                sourse.Play();
+            }
         }
         else
         {
@@ -35,7 +46,16 @@ public class ThirdPersonCharacterController : MonoBehaviour
             float ver = Input.GetAxis("Vertical");
             Vector3 playerMovement = new Vector3(hor, 0f, ver).normalized * Speed * Time.deltaTime;
             transform.Translate(playerMovement, Space.Self);
+            if (!sourse.isPlaying)
+            {
+                sourse.Play();
+            }
         }
+        if (!(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S)))
+        {
+            sourse.Stop();
+        }
+
        
     }
 }
